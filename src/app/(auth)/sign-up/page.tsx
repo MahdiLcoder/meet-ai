38f600ed1947e1cd31a112378af6d71309adfaae
MahdiLcoder <labidimahdi95@gmail.com>
2025-06-20 12:27:53 +0100

    04-auth-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpView } from '@/modules/auth/ui/views/SignUpView'
+import React from 'react'
+
+function page() {
+    return (
+        <SignUpView />
+    )
+}
+
+export default page
