32e97ee64bf142dbe09ed267fd5c11fc1a73e127
MahdiLcoder <labidimahdi95@gmail.com>
2025-06-21 10:33:45 +0100

    09 agent setup

---
+++ src/modules/agents/ui/views/AgentsView.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import ErrorState from "@/components/ErrorState"
+import LoadingState from "@/components/LoadingState"
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+
+type Props = {}
+
+function AgentsView({ }: Props) {
+
+    const trpc = useTRPC()
+    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions())
+
+
+    return (
+        <div>
+            {JSON.stringify(data, null, 2)}
+        </div>
+    )
+}
+
+export default AgentsView
+
+
+export const AgentsViewLoading = () => {
+
+    return (
+        <LoadingState title="Loading Agents" description="This may take a few seconds" />
+    )
+}
+
+export const AgentsViewError = () => {
+
+    return (
+        <ErrorState title="Error Loading Agents" description="Something went wrong" />
+    )
+}
